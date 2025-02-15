9d6dc516d328e3d48d594b79168f89854f772742
Space <neurospace369@gmail.com>
2025-02-14 16:42:20 -0800

    Initial EternumIDO commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,6 @@
-require("@nomicfoundation/hardhat-ethers");
-require("@nomicfoundation/hardhat-verify");
-require("dotenv").config();
-
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.20",  // Matches OpenZeppelin version
   networks: {
-    polygon: {
-      url: process.env.POLYGON_RPC,
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY.trim()] : [],
-    },
-  },
-  etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY,
+    hardhat: {},
   },
 };
